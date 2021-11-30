9bd4f257e12734b68c61db8de59e8b3227b03604
Adrian Barnes <adrian@tenantinc.com>
2021-11-30 15:44:06 -0800

    pre-vuetify

---
+++ src/stories/HbIcon.stories.js
@@ -0,0 +1,29 @@
+import HbIcon from '../lib-components/HbIcon.vue';
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Hb-Icon',
+  component: HbIcon,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { HbIcon },
+  template: '<hb-icon/>',
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Icon',
+};
